7007408593d5255846b9601694112669a1edb2a6
busi-n <khulisanimsipa@gmail.com>
2026-04-09 15:57:39 +0200

    created array to hold teams

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.topsoccerteams
 
+import android.hardware.biometrics.BiometricManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    val teams =  arrayOf<String> ( "Mam Sundowns FC", " Orlando Pirates", "Bidvest Wits",
+        "Stellenbosch FC", " Sek united FC")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
