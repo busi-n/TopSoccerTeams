df59b5c61232fd220b9a72aadd280b25b66153f5
busi-n <khulisanimsipa@gmail.com>
2026-04-09 16:08:44 +0200

    displaying array content in text view

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.example.topsoccerteams
 
+import android.annotation.SuppressLint
 import android.hardware.biometrics.BiometricManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
 
 class MainActivity : AppCompatActivity() {
 
 
     val teams =  arrayOf<String> ( "Mam Sundowns FC", " Orlando Pirates", "Bidvest Wits",
         "Stellenbosch FC", " Sek united FC")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val teamsTxt = findViewById<TextView>(R.id.teamsTxt)
+
+        // re-assigning position 0 of the teams array
+        teams[0] = "Mam Sundowns FC :)"
+        //displayin top soccer team in text view on UI
+        teamsTxt.text = Arrays.toString(teams)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
